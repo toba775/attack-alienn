d160ff4593c1b4180157128c58caa0f9baebcd90
toba775 <ifetoba.ethan@gmail.com>
2025-05-13 18:22:19 +0100

    Add files via upload

--- alienshoot.py
+++ alienshoot.py
@@ -5,9 +5,10 @@ HEIGHT = 400
 WIDTH = 400
 points = 0
 alien = Actor("alien")
+color = "black"
 t = "shoot"
 def draw():
-    screen.fill("black")
+    screen.fill(color)
     alien.draw()
     screen.draw.text(t,(300,50))
     screen.draw.text(str(points),(50,350))
@@ -15,14 +16,17 @@ def draw():
 def on_mouse_down(pos):
     global t
     global points
+    global color
     if alien.collidepoint(pos):
         alien.x = random.randint(0,400)
         alien.y = random.randint(0,400)
         t = "Good shot"
         points += 5
+        color = "green"
     else:
         t = "missed loser"
         points -= 50
+        color = "red"
     
 
 pgzrun.go()
